d39aed7165c2cd17c3cc3245bbd483e9f9670727
Julys Martins <julystube@gmail.com>
2021-04-29 19:17:55 -0300

    new config

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -2,9 +2,7 @@ import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
   * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+
   }
   html {
     font-size: 62.5%;
@@ -26,4 +24,4 @@ export const GlobalStyles = createGlobalStyle`
   a {
 
   }
-
+`;
